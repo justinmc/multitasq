eec7ef487e840db1427cdf33d0217ba25aeb94d7
justinmc <jmccandless@local-motors.com>
2014-02-20 20:18:16 -0700

    basic gulpfile for app and dist basics, but the compiled dist wont work yet

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,36 @@
 var gulp = require('gulp');
-var gutil = require('gulp-util');
 
-gulp.task('default', function(){
-  // place code for your default task here
+var clean = require('gulp-clean');
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+
+var paths = {
+    scripts: ['app/js/**/*.js', '!app/js/libs/**/*.js'],
+    styles: ['app/css/**/*.css'],
+    html: ['app/**/*.html'], 
+    dist: 'dist/'
+};
+
+gulp.task('clean', function() {
+    return gulp.src(paths.dist)
+        .pipe(clean());
 });
 
+gulp.task('scripts', function() {
+    return gulp.src(paths.scripts)
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(uglify())
+        .pipe(concat('multitasq.min.js'))
+        .pipe(gulp.dest(paths.dist + 'scripts/'));
+});
+
+gulp.task('copy', function() {
+    return gulp.src(paths.html.concat(paths.styles))
+        .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('default', ['clean', 'scripts', 'copy']);
 
