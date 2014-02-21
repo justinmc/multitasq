438bab0be571ce814d7c17242f0d50ff0803fe99
justinmc <jmccandless@local-motors.com>
2014-02-20 21:15:36 -0700

    moved startup code into app.js

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+$(function(){
+    // create the app
+    var multitasq = new Multitasq_Sandbox();
+
+    // help button listener
+    $(".helpToggle").on("click", function() {
+        if ($("#content_overlay").css("display") == "block") {
+            multitasq.render();
+            $("#content_tasksvg").show();
+            $("#content_overlay").hide();
+        }
+        else {
+            multitasq.clear();
+            $("#content_tasksvg").hide();
+            $("#content_overlay").show();
+        }
+    });
+});
