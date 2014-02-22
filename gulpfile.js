e302b16597e8d820fa812a49725d5708ec3ff2aa
Justin McCandless <justinjmccandless@gmail.com>
2014-02-21 23:27:51 -0700

    cleaned up gulpfile with cwd

--- gulpfile.js
+++ gulpfile.js
@@ -5,54 +5,57 @@ var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
-var paths = {
-    scripts: ['app/scripts/model/task.js', 'app/scripts/collection/tasks.js', 'app/scripts/view/sandbox.js', 'app/scripts/view/task.js', 'app/scripts/app.js'],
-    styles: ['app/styles/**/*.css'],
-    html: ['app/index.html', 'app/404.html'], 
-    bower: ['bower_components/jquery/dist/jquery.js', 'bower_components/underscore/underscore.js', 'bower_components/backbone/backbone.js', 'bower_components/backbone.localStorage/backbone.localStorage-min.js'],
-    extras: ['app/crossdomain.xml', 'app/humans.txt', 'app/manifest.appcache', 'app/robots.txt', 'app/favicon.ico'],
+var bases = {
     app: 'app/',
-    dist: 'dist/'
+    dist: 'dist/',
+};
+
+var paths = {
+    scripts: ['scripts/model/task.js', 'scripts/collection/tasks.js', 'scripts/view/sandbox.js', 'scripts/view/task.js', 'scripts/app.js'],
+    styles: ['styles/**/*.css'],
+    html: ['index.html', '404.html'], 
+    bower: ['bower_components/jquery/dist/jquery.js', 'bower_components/underscore/underscore.js', 'bower_components/backbone/backbone.js', 'bower_components/backbone.localStorage/backbone.localStorage-min.js'],
+    extras: ['crossdomain.xml', 'humans.txt', 'manifest.appcache', 'robots.txt', 'favicon.ico'],
 };
 
 gulp.task('clean', function() {
-    return gulp.src(paths.dist)
+    return gulp.src(bases.dist)
         .pipe(clean());
 });
 
 gulp.task('scripts', function() {
-    return gulp.src(paths.scripts)
+    return gulp.src(paths.scripts, {cwd: bases.app})
         .pipe(jshint())
         .pipe(jshint.reporter('default'))
         .pipe(uglify())
         .pipe(concat('multitasq.min.js'))
-        .pipe(gulp.dest(paths.dist + 'scripts/'));
+        .pipe(gulp.dest(bases.dist + 'scripts/'));
 });
 
 /* Not needed for favicon
 gulp.task('imagemin', function() {
     return gulp.src(paths.images)
         .pipe(imagemin())
-        .pipe(gulp.dest(paths.dist));
+        .pipe(gulp.dest(bases.dist));
 });
 */
 
 gulp.task('copy', function() {
     // copy html
-    gulp.src(paths.html)
-        .pipe(gulp.dest(paths.dist));
+    gulp.src(paths.html, {cwd: bases.app})
+        .pipe(gulp.dest(bases.dist));
 
     // copy styles
-    gulp.src(paths.styles)
-        .pipe(gulp.dest(paths.dist + 'styles'));
+    gulp.src(paths.styles, {cwd: bases.app})
+        .pipe(gulp.dest(bases.dist + 'styles'));
 
     // copy bower scripts
     gulp.src(paths.bower, {cwd: 'app/**'})
-        .pipe(gulp.dest(paths.dist));
+        .pipe(gulp.dest(bases.dist));
 
     // copy extra html5bp files
-    gulp.src(paths.extras)
-        .pipe(gulp.dest(paths.dist));
+    gulp.src(paths.extras, {cwd: bases.app})
+        .pipe(gulp.dest(bases.dist));
 });
 
 gulp.task('watch', function() {
