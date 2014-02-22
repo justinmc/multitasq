d55aa7c8c88d879cb9aa29a69fc2b39ec2206235
Justin McCandless <justinjmccandless@gmail.com>
2014-02-21 23:10:17 -0700

    dist working\! cleaned up unnecessary files

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,13 @@ var clean = require('gulp-clean');
 var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var imagemin = require('gulp-imagemin');
 
 var paths = {
     scripts: ['app/scripts/model/task.js', 'app/scripts/collection/tasks.js', 'app/scripts/view/sandbox.js', 'app/scripts/view/task.js', 'app/scripts/app.js'],
     styles: ['app/styles/**/*.css'],
     html: ['app/index.html', 'app/404.html'], 
+    bower: ['bower_components/jquery/dist/jquery.js', 'bower_components/underscore/underscore.js', 'bower_components/backbone/backbone.js', 'bower_components/backbone.localStorage/backbone.localStorage-min.js'],
+    extras: ['app/crossdomain.xml', 'app/humans.txt', 'app/manifest.appcache', 'app/robots.txt', 'app/favicon.ico'],
     app: 'app/',
     dist: 'dist/'
 };
@@ -25,11 +26,32 @@ gulp.task('scripts', function() {
         .pipe(jshint.reporter('default'))
         .pipe(uglify())
         .pipe(concat('multitasq.min.js'))
-        .pipe(gulp.dest(paths.app + 'scripts/'));
+        .pipe(gulp.dest(paths.dist + 'scripts/'));
 });
 
+/* Not needed for favicon
+gulp.task('imagemin', function() {
+    return gulp.src(paths.images)
+        .pipe(imagemin())
+        .pipe(gulp.dest(paths.dist));
+});
+*/
+
 gulp.task('copy', function() {
-    return gulp.src(paths.html.concat(paths.styles))
+    // copy html
+    gulp.src(paths.html)
+        .pipe(gulp.dest(paths.dist));
+
+    // copy styles
+    gulp.src(paths.styles)
+        .pipe(gulp.dest(paths.dist + 'styles'));
+
+    // copy bower scripts
+    gulp.src(paths.bower, {cwd: 'app/**'})
+        .pipe(gulp.dest(paths.dist));
+
+    // copy extra html5bp files
+    gulp.src(paths.extras)
         .pipe(gulp.dest(paths.dist));
 });
 
