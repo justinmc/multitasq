10ba68e92ee24b6b360837e55dc244208afa6522
Justin McCandless <justinjmccandless@gmail.com>
2014-03-15 15:43:19 -0700

    created a nicer app object

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,18 +1,54 @@
-$(function(){
-    // create the app
-    var multitasq = new Multitasq_Sandbox();
+var Multitasq = (function() {
 
-    // help button listener
-    $(".helpToggle").on("click", function() {
-        if ($("#content_overlay").css("display") === "block") {
-            multitasq.render();
-            $("#content_tasksvg").show();
-            $("#content_overlay").hide();
-        }
-        else {
-            multitasq.clear();
-            $("#content_tasksvg").hide();
-            $("#content_overlay").show();
-        }
-    });
-});
+    Multitasq.prototype.sandbox = null;
+
+    Multitasq.prototype.helpShown = false;
+
+    // Constructor
+    function Multitasq() {
+
+        var that = this;
+
+        $(function(){
+            that.sandbox = new Multitasq_Sandbox();
+            that.createEvents();
+        });
+    }
+
+    // Sets events
+    Multitasq.prototype.createEvents = function() {
+
+        var that = this;
+
+        // help dialog toggle buttons
+        $(".helpToggle").on("click", function() {
+            if (that.helpShown) {
+                that.helpHide();
+            }
+            else {
+                that.helpShow();
+            }
+        });
+    };
+
+    // Show the help dialog and hide the app
+    Multitasq.prototype.helpShow = function() {
+        this.sandbox.clear();
+        $("#content_tasksvg").hide();
+        $("#content_overlay").show();
+        this.helpShown = true;
+    };
+
+    // Hide the help dialog and show the app
+    Multitasq.prototype.helpHide = function() {
+        this.sandbox.render();
+        $("#content_tasksvg").show();
+        $("#content_overlay").hide();
+        this.helpShown = false;
+    };
+
+    return Multitasq;
+})();
+
+// Auto-initialize the app
+new Multitasq();
